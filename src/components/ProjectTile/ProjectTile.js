93978fa3cde5fa3bc73937dcf8459cd8b5ebab11
Lance Althouse <lancealthouse@outlook.com>
2023-08-20 12:48:18 -0500

    Updated font size and some project content

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -2,13 +2,13 @@ import { React } from "react";
 
 const ProjectTile = (props) => {
   return (
-    <div className="bg-zinc-950 rounded-2xl w-80 h-96">
+    <div className="bg-zinc-950 rounded-2xl w-80 h-96 text-white">
       <img
-        className="w-60 h-60"
+        className="w-52 h-52"
         src={props.projectGifUrl}
         alt="project-gif"
       ></img>
-      <h1>{props.projectTitle}</h1>
+      <h1 className="text-center font-bold">{props.projectTitle}</h1>
     </div>
   );
 };
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -4,10 +4,10 @@ import ProjectTile from "../ProjectTile/ProjectTile";
 const Projects = () => {
   return (
     <div className="font-mono font-medium Consolas ">
-      <h1 className="font-md text-3xl text-white">projects</h1>
+      <h1 className="font-bold text-3xl text-white text-center underline pt-6">Projects</h1>
       <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-16">
-        <ProjectTile projectGifUrl="" projectTitle="" />
-        <ProjectTile projectGifUrl="" projectTitle="" />
+        <ProjectTile projectGifUrl="" projectTitle="Movie Catalog" />
+        <ProjectTile projectGifUrl="" projectTitle="Expense Splitter App" />
         <ProjectTile projectGifUrl="" projectTitle="" />
         <ProjectTile projectGifUrl="" projectTitle="" />
         <ProjectTile projectGifUrl="" projectTitle="" />
