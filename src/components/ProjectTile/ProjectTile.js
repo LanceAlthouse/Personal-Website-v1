4c455e8445caf27ed3f73846814a184aea6778e6
Lance Althouse <lancealthouse@outlook.com>
2023-08-21 11:08:32 -0500

    Slightly more advanced tile design

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -2,13 +2,14 @@ import { React } from "react";
 
 const ProjectTile = (props) => {
   return (
-    <div className="bg-zinc-950 rounded-2xl w-80 h-96 text-white">
-      <img
-        className="w-52 h-52"
+    <div className="bg-zinc-950 rounded-2xl w-80 h-96 text-white hover:border-green-600 hover:border-2 transform -skew-y-6">
+        <img
+        className="w-80 h-52 p-4"
         src={props.projectGifUrl}
         alt="project-gif"
       ></img>
       <h1 className="text-center font-bold">{props.projectTitle}</h1>
+      <p className="p-4 text-green-600">{props.projectDescription}</p>
     </div>
   );
 };
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -6,12 +6,8 @@ const Projects = () => {
     <div className="font-mono font-medium Consolas ">
       <h1 className="font-bold text-3xl text-white text-center underline pt-6">Projects</h1>
       <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-16">
-        <ProjectTile projectGifUrl="" projectTitle="Movie Catalog" />
-        <ProjectTile projectGifUrl="" projectTitle="Expense Splitter App" />
-        <ProjectTile projectGifUrl="" projectTitle="" />
-        <ProjectTile projectGifUrl="" projectTitle="" />
-        <ProjectTile projectGifUrl="" projectTitle="" />
-        <ProjectTile projectGifUrl="" projectTitle="" />
+        <ProjectTile projectGifUrl="https://imgur.com/0kv2TkZ.png" projectTitle="Expense Splitter Web App" projectDescription=""/>
+        <ProjectTile projectGifUrl="" projectTitle="Movie Catalog Web App" projectDescription="Application is currently a WIP"/>
       </div>
     </div>
   );
