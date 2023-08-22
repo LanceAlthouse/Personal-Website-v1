585c8b764ca03a6d5b406ca090b625b4c4469046
Lance Althouse <lancealthouse@outlook.com>
2023-08-22 09:43:30 -0500

    Fully implemented project tiles may add animations down the line

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -2,14 +2,18 @@ import { React } from "react";
 
 const ProjectTile = (props) => {
   return (
-    <div className="bg-zinc-950 rounded-2xl w-80 h-96 text-white hover:border-green-600 hover:border-2 transform -skew-y-6">
-        <img
-        className="w-80 h-52 p-4"
-        src={props.projectGifUrl}
-        alt="project-gif"
-      ></img>
-      <h1 className="text-center font-bold">{props.projectTitle}</h1>
-      <p className="p-4 text-green-600">{props.projectDescription}</p>
+    <div className="rounded-2xl w-80 h-96 text-white bg-gradient-to-r p-[1px] from-green-600 to-fuchsia-950 transform -skew-y-6">
+      <div className="rounded-2xl w-80 h-96 bg-zinc-950 p-3">
+        <div className="rounded-2xl bg-zinc-500">
+          <img
+            className="h-full w-full max-h-[200px] rounded-2xl p-[1px]"
+            src={props.projectImgUrl}
+            alt="project-img"
+          ></img>
+        </div>
+        <h1 className="text-center font-bold pt-2">{props.projectTitle}</h1>
+        <p className="p-4 text-green-600 text-sm">{props.projectDescription}</p>
+      </div>
     </div>
   );
 };
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -6,8 +6,8 @@ const Projects = () => {
     <div className="font-mono font-medium Consolas ">
       <h1 className="font-bold text-3xl text-white text-center underline pt-6">Projects</h1>
       <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-16">
-        <ProjectTile projectGifUrl="https://imgur.com/0kv2TkZ.png" projectTitle="Expense Splitter Web App" projectDescription=""/>
-        <ProjectTile projectGifUrl="" projectTitle="Movie Catalog Web App" projectDescription="Application is currently a WIP"/>
+        <ProjectTile projectImgUrl="https://imgur.com/0kv2TkZ.png" projectTitle="Expense Splitter Web App" projectDescription="This application is designed to streamline the process of splitting expenses among groups of people. It was collaboratively built using Flask, Python, JS, and HTML/CSS."/>
+        <ProjectTile projectImgUrl="https://placehold.co/600x400?text=WIP" projectTitle="Movie Catalog Web App" projectDescription="This application is currently a WIP."/>
       </div>
     </div>
   );
