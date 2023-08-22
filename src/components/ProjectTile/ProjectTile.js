168e7e8d9182ea81fc1e00187867ce8841856981
Lance Althouse <lancealthouse@outlook.com>
2023-08-22 17:22:54 -0500

    Changed image border to same gradient

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -3,9 +3,9 @@ import GithubLogo from "../../assets/images/github_logo.png";
 
 const ProjectTile = (props) => {
   return (
-    <div className="rounded-2xl w-80 h-[124] text-white bg-gradient-to-r px-[1px] py-[1px] from-green-600 to-fuchsia-900">
+    <div className="rounded-2xl w-80 h-[124] text-white px-[1px] py-[1px] bg-gradient-to-r from-green-600 to-fuchsia-900">
       <div className="rounded-2xl w-full h-full bg-zinc-950 p-3 overflow-hidden">
-        <div className="rounded-2xl bg-zinc-500">
+        <div className="rounded-2xl bg-gradient-to-r from-green-600 to-fuchsia-900">
           <img
             className="h-full w-full max-h-[200px] rounded-2xl p-[1px]"
             src={props.projectImgUrl}
