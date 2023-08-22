76197faaa0366ba80e9b311c06f880cd6a0af639
Lance Althouse <lancealthouse@outlook.com>
2023-08-22 09:54:21 -0500

    Removed skew from tiles

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -2,8 +2,8 @@ import { React } from "react";
 
 const ProjectTile = (props) => {
   return (
-    <div className="rounded-2xl w-80 h-96 text-white bg-gradient-to-r p-[1px] from-green-600 to-fuchsia-950 transform -skew-y-6">
-      <div className="rounded-2xl w-80 h-96 bg-zinc-950 p-3">
+    <div className="rounded-2xl w-80 h-96 text-white bg-gradient-to-r px-[1px] py-[1px] from-green-600 to-fuchsia-900">
+      <div className="rounded-2xl w-full h-full bg-zinc-950 p-3">
         <div className="rounded-2xl bg-zinc-500">
           <img
             className="h-full w-full max-h-[200px] rounded-2xl p-[1px]"
