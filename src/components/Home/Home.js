9991f221a02ba17b0de85445747c3eadb33e6228
Lance Althouse <lancealthouse@outlook.com>
2023-08-24 14:48:56 -0500

    Updated home page to sound better

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="font-mono font-medium Consolas">
-      <h1 className="font-md text-3xl text-white text-center lg:pt-96 left-auto">
-        Hi, my name is Lance Althouse
-      </h1>
-      <p className="text-white text-center mt-4">
-        I enjoy programming and working on my homelab in my free time.
-      </p>
+    <div className="font-mono font-medium Consolas flex flex-col items-center justify-center">
+      <div className="text-left lg:pt-96">
+        <h1 className="text-4xl text-white">
+          Hello, I'm <span className="text-green-600">Lance Althouse</span>,
+        </h1>
+        <h2 className="text-3xl text-white">
+          an enthusiast in software engineering and homelab building.
+        </h2>
+      </div>
     </div>
   );
 };
