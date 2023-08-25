e213035ae60f2e851eb2b3174c0a541ef3c3a384
Lance Althouse <lancealthouse@outlook.com>
2023-08-24 22:42:36 -0500

    Added some small animation changes

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -3,7 +3,7 @@ import GithubLogo from "../../assets/images/github_logo.png";
 
 const ProjectTile = (props) => {
   return (
-    <div className="rounded-2xl w-80 h-[124] text-white px-[1px] py-[1px] bg-gradient-to-r from-green-600 to-fuchsia-900">
+    <div className="rounded-2xl w-80 h-[124] text-white px-[1px] py-[1px] bg-gradient-to-r from-green-600 to-fuchsia-900 hover:scale-110 ease-in duration-300 shadow-lg shadow-black">
       <div className="rounded-2xl w-full h-full bg-zinc-950 p-3 overflow-hidden">
         <div className="rounded-2xl bg-gradient-to-r from-green-600 to-fuchsia-900">
           <img
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -4,10 +4,7 @@ import ProjectTile from "../ProjectTile/ProjectTile";
 const Projects = () => {
   return (
     <div className="font-mono font-medium Consolas ">
-      <h1 className="font-bold text-3xl text-white text-center underline pt-6">
-        Projects
-      </h1>
-      <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-16 h-[650px]">
+      <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-8 h-[750px]">
         <ProjectTile
           projectImgUrl="https://imgur.com/0kv2TkZ.png"
           projectGithubUrl="https://github.com/isaiahstone21/Expense-Splitter"
