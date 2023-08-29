16006f05a1e953a4b32ddbc24c7028485cf1b61c
Lance Althouse <lancealthouse@outlook.com>
2023-08-28 21:40:25 -0500

    Implemented contact page - but no backend yet

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,9 +1,107 @@
-import { React } from "react";
+import React, { useState } from "react";
+import PersonIcon from "../../assets/images/person_icon.png";
+import EmailIcon from "../../assets/images/email_icon.png";
+import MessageIcon from "../../assets/images/message_icon.png";
 
 const ContactSection = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [formSubmission, setFormSubmission] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Need to implement a way to send the form data to my email
+
+    setFormSubmission(
+      `Thank you for your submission ${name}, I will get back to you as soon as possible.`
+    );
+    console.log("Form submitted:", { name, email, message });
+    // Reset the form
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <div className="font-mono font-medium Consolas">
-      <h1 className="font-md text-3xl text-white">contact</h1>
+    <div className="font-mono font-medium Consolas pt-48">
+      <h1 className="text-3xl font-bold text-green-600 text-center underline">
+        Contact Me
+      </h1>
+      <form
+        className="pt-4 space-y-4 max-w-xl mx-auto text-white"
+        onSubmit={handleSubmit}
+      >
+        <div className="pt-4 flex h-16">
+          <span className="bg-gray-800 rounded-l-lg w-14 flex justify-center items-center border border-l-gray-500 border-r-0 border-t-gray-500 border-b-gray-500">
+            <img src={PersonIcon} alt="user icon" className="w-6 h-6"></img>
+          </span>
+          <input
+            type="text"
+            id="name"
+            className="w-full px-4 py-2 rounded-r-lg border border-l-0 border-r-gray-500 border-t-gray-500 border-b-gray-500 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-600 shadow-lg shadow-black"
+            value={name}
+            autoComplete="name"
+            placeholder="Name"
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className="pt-4 flex h-16">
+          <span className="bg-gray-800 rounded-l-lg w-14 flex justify-center items-center border border-l-gray-500 border-r-0 border-t-gray-500 border-b-gray-500">
+            <img src={EmailIcon} alt="email icon" className="w-6 h-6"></img>
+          </span>
+          <input
+            type="email"
+            id="email"
+            className="w-full px-4 py-2 rounded-r-lg border border-l-0 border-r-gray-500 border-t-gray-500 border-b-gray-500 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-600 shadow-lg shadow-black"
+            value={email}
+            autoComplete="email"
+            placeholder="Email"
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="pt-4 flex h-32">
+          <span className="bg-gray-800 rounded-l-lg w-14 flex justify-center items-center border border-l-gray-500 border-r-0 border-t-gray-500 border-b-gray-500">
+            <img src={MessageIcon} alt="message icon" className="w-6 h-6"></img>
+          </span>
+          <textarea
+            id="message"
+            className="w-full px-4 py-2 rounded-r-lg border border-l-0 border-r-gray-500 border-t-gray-500 border-b-gray-500 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-600 shadow-lg shadow-black"
+            value={message}
+            autoComplete="off"
+            placeholder="Write your message here..."
+            onChange={handleMessageChange}
+            rows={6}
+            style={{ resize: "none" }}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-center pr-4">
+          <button
+            type="submit"
+            className="px-4 py-2 text-white bg-gray-600 rounded-lg hover:bg-green-600 hover:opacity-100 shadow-lg shadow-black"
+          >
+            Submit
+          </button>
+        </div>
+        <div className="text-center text-2xl pt-4 text-green-600">
+          {formSubmission}
+        </div>
+      </form>
     </div>
   );
 };
