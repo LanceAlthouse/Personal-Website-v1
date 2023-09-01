f536f82bc99f1c3757c02c59d010eff123367c39
Lance Althouse <lancealthouse@outlook.com>
2023-08-31 19:53:38 -0500

    Added to index for preloading images

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -9,13 +9,13 @@ const Projects = () => {
       <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-8 pt-16">
         <ProjectTile
           projectImgUrl={ExpenseSplitterImg}
-          projectGithubUrl="https://github.com/isaiahstone21/Expense-Splitter"
+          projectGithubUrl="https://github.com/Lancealot4567/Expense-Splitter"
           projectTitle="Expense Splitter Web App"
           projectDescription="This application was developed in collaboration with one other individual and is aimed at simplifying the procedure of distributing costs within a group of people. The collaborative effort utilized Flask, Python, JS, and HTML/CSS for its construction."
         />
         <ProjectTile
           projectImgUrl={WorkInProgressImg}
-          projectGithubUrl="https://github.com/isaiahstone21/Movie-Catalog"
+          projectGithubUrl="https://github.com/Lancealot4567/Movie-Catalog"
           projectTitle="Movie Catalog Web App"
           projectDescription="This application is currently a WIP."
         />
