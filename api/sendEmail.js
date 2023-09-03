81c8993d51cbd4f3764f5892fbaa171b1989ba29
Lance Althouse <lancealthouse@outlook.com>
2023-09-02 21:58:41 -0500

    Potential rate limiting for emails

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -4,6 +4,7 @@ const smtpHost = process.env.SMTP_HOST;
 const smtpUser = process.env.SMTP_USER;
 const smtpPass = process.env.SMTP_PASS;
 const forwardEmail = process.env.FORWARD_EMAIL;
+const rateLimit = require("express-rate-limit");
 
 const transporter = nodemailer.createTransport({
   host: smtpHost,
@@ -14,21 +15,31 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-module.exports = async (req, res) => {
-  const { from, to, subject, text } = req.body;
-  const mailOptions = {
-    from: smtpUser,
-    to: forwardEmail,
-    subject: subject,
-    text: text,
-  };
+const limiter = rateLimit({
+  windowMs: 86400,
+  max: 2,
+  handler: (req, res) => {
+    res.status(429).json({ message: "Rate limit exceeded" });
+  },
+});
 
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", info.response);
-    res.status(200).json({ message: "Email sent successfully" });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+module.exports = async (req, res) => {
+  limiter(req, res, async () => {
+    const { from, to, subject, text } = req.body;
+    const mailOptions = {
+      from: smtpUser,
+      to: forwardEmail,
+      subject: subject,
+      text: text,
+    };
+
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Email sent:", info.response);
+      res.status(200).json({ message: "Email sent successfully" });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
 };
