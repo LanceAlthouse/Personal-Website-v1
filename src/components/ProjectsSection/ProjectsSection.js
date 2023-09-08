e081829e137ea9a8885abfb6297ff5145cd90c43
Lance Althouse <lancealthouse@outlook.com>
2023-09-07 19:07:45 -0500

    Cleaned up/refactored projectsection component

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,51 +1,22 @@
 import { React } from "react";
+import { projectData } from "../../data/projectData";
 import ProjectTile from "../ProjectTile/ProjectTile";
-import ExpenseSplitterImg from "../../assets/images/expense_splitter_project_image.avif";
-import WorkInProgressImg from "../../assets/images/work_in_progress_image.avif";
 
 const Projects = () => {
+  const projectTiles = projectData.map((project) => (
+    <ProjectTile
+      projectImgUrl={project.projectImgUrl}
+      projectGithubUrl={project.projectGithubUrl}
+      projectTitle={project.projectTitle}
+      projectStatus={project.projectStatus}
+      projectDescription={project.projectDescription}
+    />
+  ));
+
   return (
     <div className="font-mono font-medium Consolas">
       <div className="flex flex-wrap items-center justify-center grid-cols-5 grid-rows-5 gap-16 p-8 pt-16">
-        <ProjectTile
-          projectImgUrl={WorkInProgressImg}
-          projectGithubUrl="https://github.com/Lancealot4567/Movie-Catalog"
-          projectTitle="Movie Catalog Web App"
-          projectStatus="Work In Progress"
-          projectDescription="This application is currently a WIP."
-        />
-        <ProjectTile
-          projectImgUrl={WorkInProgressImg}
-          projectGithubUrl="https://github.com/Lancealot4567/Personal-Website"
-          projectTitle="Personal Portfolio Website"
-          projectStatus="Work In Progress"
-          projectDescription="This application is currently a WIP."
-        />
-        <ProjectTile
-          projectImgUrl={ExpenseSplitterImg}
-          projectGithubUrl="https://github.com/Lancealot4567/Expense-Splitter"
-          projectTitle="Expense Splitter Web App"
-          projectStatus="Completed"
-          projectDescription="This application was developed in collaboration with one other individual and is aimed at simplifying the procedure of distributing costs within a group of people. The collaborative effort utilized Flask, Python, JS, and HTML/CSS for its construction."
-        />
-        <ProjectTile
-          projectImgUrl={WorkInProgressImg}
-          projectTitle="Burnplan Evaluation App"
-          projectStatus="Completed"
-          projectDescription=""
-        />
-        <ProjectTile
-          projectImgUrl={WorkInProgressImg}
-          projectTitle="Travel Planner App"
-          projectStatus="Completed"
-          projectDescription=""
-        />
-        <ProjectTile
-          projectImgUrl={WorkInProgressImg}
-          projectTitle="Sports Weather App"
-          projectStatus="Completed"
-          projectDescription=""
-        />
+        {projectTiles}
       </div>
     </div>
   );
---
+++ src/data/projectData.js
@@ -0,0 +1,45 @@
+import ExpenseSplitterImg from "../assets/images/expense_splitter_project_image.avif";
+import WorkInProgressImg from "../assets/images/work_in_progress_image.avif";
+
+export const projectData = [
+  {
+    projectImgUrl: WorkInProgressImg,
+    projectGithubUrl: "https://github.com/Lancealot4567/Movie-Catalog",
+    projectTitle: "Movie Catalog Web App",
+    projectStatus: "Work In Progress",
+    projectDescription: "This application is currently a WIP.",
+  },
+  {
+    projectImgUrl: WorkInProgressImg,
+    projectGithubUrl: "https://github.com/Lancealot4567/Personal-Website",
+    projectTitle: "Personal Portfolio Website",
+    projectStatus: "Work In Progress",
+    projectDescription: "This application is currently a WIP.",
+  },
+  {
+    projectImgUrl: ExpenseSplitterImg,
+    projectGithubUrl: "https://github.com/Lancealot4567/Expense-Splitter",
+    projectTitle: "Expense Splitter Web App",
+    projectStatus: "Completed",
+    projectDescription:
+      "This application was developed in collaboration with one other individual and is aimed at simplifying the procedure of distributing costs within a group of people. The collaborative effort utilized Flask, Python, JS, and HTML/CSS for its construction.",
+  },
+  {
+    projectImgUrl: WorkInProgressImg,
+    projectTitle: "Burnplan Evaluation App",
+    projectStatus: "Completed",
+    projectDescription: "",
+  },
+  {
+    projectImgUrl: WorkInProgressImg,
+    projectTitle: "Travel Planner App",
+    projectStatus: "Completed",
+    projectDescription: "",
+  },
+  {
+    projectImgUrl: WorkInProgressImg,
+    projectTitle: "Sports Weather App",
+    projectStatus: "Completed",
+    projectDescription: "",
+  },
+];
